239aa86550c3ddc00a9ae6e0328f5af008b9cc1f
William Okech <38021234+wokech@users.noreply.github.com>
2024-11-25 13:14:48 +0300

    Added data files for email course

---
+++ r_scripts_for_images/my_arithmetic_1.R
@@ -0,0 +1,25 @@
+# Basic Arithmetic Calculations
+
+# Addition
+10 + 30
+
+# Subtraction
+30 -24
+
+# Multiplication
+20 * 4
+
+# Division
+93 / 4
+
+# Exponentiation
+3^6
+
+# Modulus (remainder with division)
+94 %% 5
+
+# Integer Division
+54 %/% 7
+
+# Slightly more complex arithmetic operations
+5 - 1 + (4 * 3) / 16 * 3
---
+++ r_scripts_for_images/my_variables_1.R
@@ -0,0 +1,23 @@
+# Variable assignment
+
+# Using the "<-" assignment operator
+
+variable_1 <- 5
+variable_1 
+
+# Using the "=" assignment operator
+
+variable_2 <- 10
+variable_2 
+
+# The order of the value and the variable can be switched 
+# with the "->" assignment operator
+
+15 -> variable_3
+variable_3
+
+# Two variables can be assigned to one value
+
+variable_4 <- variable_5 <- 30
+variable_4
+variable_5
